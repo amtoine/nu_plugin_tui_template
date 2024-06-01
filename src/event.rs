b983b07ccd838227f65d9a9ecd817516a01e2f35
amtoine <stevan.antoine@gmail.com>
2024-06-01 13:45:25 +0200

    fix channel error

--- src/event.rs
+++ src/event.rs
@@ -44,19 +44,23 @@ impl EventHandler {
                         .unwrap_or(tick_rate);
 
                     if event::poll(timeout).expect("failed to poll new events") {
-                        match event::read().expect("unable to read event") {
+                        let res = match event::read().expect("unable to read event") {
                             CrosstermEvent::Key(e) => sender.send(Event::Key(e)),
                             CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
                             CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
                             CrosstermEvent::FocusGained => Ok(()),
                             CrosstermEvent::FocusLost => Ok(()),
                             CrosstermEvent::Paste(_) => unimplemented!(),
+                        };
+                        if res.is_err() {
+                            break;
                         }
-                        .expect("failed to send terminal event")
                     }
 
                     if last_tick.elapsed() >= tick_rate {
-                        sender.send(Event::Tick).expect("failed to send tick event");
+                        if sender.send(Event::Tick).is_err() {
+                            break;
+                        }
                         last_tick = Instant::now();
                     }
                 }
